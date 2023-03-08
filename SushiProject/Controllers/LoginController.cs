19b9f4361b0c73e6686c896da779b636eca83586
Jack Kessler <jack_kessler@hotmail.com>
2023-03-07 22:47:16 -0600

    Connected username and password database

---
+++ SushiProject/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SushiProject.Models;
+using SushiProject.Services.Business;
+
+namespace SushiProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Login(UserModel userModel)
+        {
+            //return $"Results: Username = {userModel.Username} PW = {userModel.Password}";
+            SecurityService securityService = new SecurityService();
+            bool success = securityService.Authenticate(userModel);
+            
+            if (success)
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure");
+            }
+        }
+
+    }
+}
