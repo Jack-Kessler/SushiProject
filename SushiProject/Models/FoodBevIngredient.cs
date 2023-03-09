f55480b5254ddf55054548e97bf42cdfcbd6d3cf
Jack Kessler <jack_kessler@hotmail.com>
2023-03-09 13:19:30 -0600

    Fully Functioning

--- SushiProject/Models/FoodBevIngredient.cs
+++ SushiProject/Models/FoodBevIngredient.cs
@@ -7,22 +7,22 @@ namespace SushiProject.Models
         public int IngredientID { get; set; }
 
         [Required (ErrorMessage = "Please enter a valid ingredient name")]
-        public string IngredientName { get; set; }
+        public string? IngredientName { get; set; }
 
 
-        [Required(ErrorMessage = "Please enter a valid initial stock level")]
+        [Required(ErrorMessage = "Please enter a valid initial stock level between 0 and 2147483647")]
         [Range (0, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}")]
-        public int IngredientStockLevel { get; set; }
+        public int? IngredientStockLevel { get; set; }
 
 
-        [Required(ErrorMessage = "Please enter a valid ingredient cost per unit")]
+        [Required(ErrorMessage = "Please enter a valid ingredient cost per unit between $0.00 - $99.00")]
         [DataType(DataType.Currency)]
         [Range(0, 99.99, ErrorMessage = "Value for {0} must be between {1} and {2}")]
-        public double IngredientCost { get; set; }
+        public double? IngredientCost { get; set; }
 
 
         [Required(ErrorMessage = "Please enter a valid category name")]
-        public string IngredientCategoryName { get; set; }
+        public string? IngredientCategoryName { get; set; }
 
 
         public IEnumerable<FoodBevIngredientCategory>? IngredientCategories { get; set; }
