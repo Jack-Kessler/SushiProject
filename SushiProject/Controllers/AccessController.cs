3478d2f61fa424193b796fc8eef1b8264cab55c4
Jack Kessler <jack_kessler@hotmail.com>
2023-03-11 08:43:51 -0600

    Access Controler Modified

--- SushiProject/Controllers/AccessController.cs
+++ SushiProject/Controllers/AccessController.cs
@@ -30,14 +30,37 @@ namespace SushiProject.Controllers
 
             if (success == true)
             {
-                
                 List<Claim> claims = new List<Claim>()
-                {
-                    new Claim(ClaimTypes.NameIdentifier, modelLogin.UserName),
-                    new Claim(ClaimTypes.Role,"Admin")
-                };
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, modelLogin.UserName),
+                    };
 
-                
+                if (modelLogin.Role == "Server")
+                {
+                    var newClaim = new Claim(ClaimTypes.Role, "Server");
+                    claims.Add(newClaim);
+                }
+                else if (modelLogin.Role == "Chef")
+                {
+                    var newClaim = new Claim(ClaimTypes.Role, "Chef");
+                    claims.Add(newClaim);
+                }
+                else if (modelLogin.Role == "Manager")
+                {
+                    var newClaim = new Claim(ClaimTypes.Role, "Manager");
+                    claims.Add(newClaim);
+                }
+                else if(modelLogin.Role == "Owner")
+                {
+                    var newClaim = new Claim(ClaimTypes.Role, "Owner");
+                    claims.Add(newClaim);
+                }
+                else
+                {
+                    ViewData["ValidateMessage"] = "Role Not Found";
+                    return View(modelLogin);
+                }
+
                 ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
                 AuthenticationProperties properties = new AuthenticationProperties()
@@ -49,15 +72,15 @@ namespace SushiProject.Controllers
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
 
                 return RedirectToAction("Index", "Home");
+
             }
             else
             {
                 //ModelState.AddModelError(string.Empty, "Invalid login attempt");
-                ViewData["ValidateMessage"] = "User Not Found";
+                ViewData["ValidateMessage"] = "User does not exist or Password is incorrect";
                 return View(modelLogin);
                 //return View("LoginFailure");
             }
-
 
             //if (modelLogin.UserName == null) { }
             //return View();
