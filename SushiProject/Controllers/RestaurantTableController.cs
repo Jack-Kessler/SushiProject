32a2cdb4cc7d73b05762ad47e797f2547547d67b
Jack Kessler <jack_kessler@hotmail.com>
2023-03-12 11:49:32 -0500

    Building out restaurant tables

---
+++ SushiProject/Controllers/RestaurantTableController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SushiProject.Models;
+
+namespace SushiProject.Controllers
+{
+    public class RestaurantTableController : Controller
+    {
+        private readonly IRestaurantTableRepository repo;
+
+        public RestaurantTableController(IRestaurantTableRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            var items = repo.GetAllRestaurantTablesSQL();
+            return View(items);
+        }
+
+        public IActionResult ViewRestaurantTable(int id)
+        {
+            var item = repo.GetRestaurantTableSQL(id);
+            return View(item);
+        }
+
+        public IActionResult UpdateRestaurantTable(int id)
+        {
+            RestaurantTable updateTable = repo.GetMenuItemSQL(id);
+           
+            if (updateTable == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(updateTable);
+        }
+
+        public IActionResult UpdateRestaurantTableToDatabase(MenuItem tableToUpdate)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.UpdateRestaurantTableSQL(tableToUpdate);
+                //return RedirectToAction("ViewMenuItem", new { id = menuItemToUpdate.MenuItemID }); Update this row
+            }
+            else
+            {
+                return View("UpdateRestaurantTable", tableToUpdate);
+            }
+        }
+
+        public IActionResult InsertRestaurantTable() //NEED TO REDO
+        {
+            var item = repo.AssignMenuItemCategorySQL();
+            var ingredients = repo.AssignMenuItemIngredientListSQL();
+            item.MenuItemIngredientList = ingredients;
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult InsertRestaurantTableToDatabase(RestaurantTable tableToInsert)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.InsertRestaurantTableSQL(tableToInsert);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("InsertRestaurantTable", tableToInsert);
+            }
+        }
+
+        public IActionResult DeleteRestaurantTable(RestaurantTable table)
+        {
+            repo.DeleteRestaurantTableSQL(table);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SushiProject/Models/RestaurantTable.cs
@@ -0,0 +1,8 @@
+﻿namespace SushiProject.Models
+{
+    public class RestaurantTable
+    {
+        public int RestaurantTableID { get; set; }
+        public int RestaurantTableAssignedEmployeeID { get; set; }
+    }
+}
